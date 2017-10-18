aac77db389ac97954caf779d0dce10143f1d580e
German <me@g3rm4n.xyz>
2017-10-17 20:30:35 -0500

    sanitize question

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,22 @@ const functions = require('firebase-functions');
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
+
+
+exports.sanitize = functions.database
+    .ref('/rooms/{roomId}/questions/{questionId}')
+    .onWrite(event => {
+        const question = event.data.val();
+        if(post.sanitized) return;
+        console.log(`Sanitizing question ${event.params.questionId}`);
+        console.log(question);
+        question.sanitzed = true;
+        question.question = sanitize(question.question);
+        return event.data.ref.set(question);
+    });
+
+    sanitize = s => {
+        let sanitatizeText = s;
+        sanitatizeText = sanitatizeText.replace(/\bstupid\b/ig, "greate");
+        return sanitatizeText;
+    }
